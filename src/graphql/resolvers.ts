e9bde2c29196dc874b958f609205186c2230d702
rhasija <ravi.hasija@gmail.com>
2021-05-01 11:34:49 -0700

    - CRUD is working!

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,6 +1,9 @@
-const { v4: uuidv4 } = require('uuid');
+import {User} from "./typedefs";
+import {ApolloError, UserInputError} from 'apollo-server-errors';
 
-const users = [
+const {v4: uuidv4} = require('uuid');
+
+let users: User[] = [
     {
         "id": "ac658bae-57c6-4f13-975a-7c3146c64a49",
         "name": "backend test",
@@ -18,15 +21,66 @@ export const graphqlResolvers = {
         getAllUsers: () => users,
     },
     Mutation: {
-        createUser: (_: any, {input} : any) => {
-            console.log(`${JSON.stringify(input)}`);
-            users.push({
-                id: uuidv4(),
-                ...input,
-                createdAt: new Date().toISOString(),
-                updatedAt: new Date().toISOString(),
-            })
-            return users[users.length - 1];
+        createUser: (_: any, {input}: any) => {
+            try {
+                users.push({
+                    id: uuidv4(),
+                    ...input,
+                    createdAt: new Date().toISOString(),
+                    updatedAt: new Date().toISOString(),
+                })
+                return users[users.length - 1];
+            } catch (e) {
+                throw new ApolloError(
+                    `Error creating user: ${e.message}`,
+                    'SERVER_ERROR',
+                    {}
+                );
+            }
+        },
+
+        updateUser: (_: any, {id, input}: any) => {
+            try {
+                const userIndex = users.findIndex((user) => user.id === id);
+                if (userIndex >= 0) {
+                    const currUser = users[userIndex];
+                    users[userIndex] = {
+                        ...input,
+                        id: currUser.id,
+                        createdAt: currUser.createdAt,
+                        updatedAt: new Date().toISOString(),
+                    };
+                    return users[userIndex];
+                }
+                throw new ApolloError(`User: ${id} not found in database`, 'DATA_NOT_FOUND', {});
+            } catch (e) {
+                throw new ApolloError(
+                    `Error updating user by id: ${id}: ${e.message}`,
+                    'SERVER_ERROR',
+                    {}
+                );
+            }
+        },
+
+        deleteUser: (_: any, {id}: any) => {
+            try {
+                const userIndex = users.findIndex((user) => user.id === id);
+                if (userIndex >= 0) {
+                    users = users.filter((users) => users.id !== id);
+                    return {
+                        result: `User with ${id} deleted`
+                    };
+                }
+                return {
+                    result: `User with ${id} not found`
+                };
+            } catch (e) {
+                throw new ApolloError(
+                    `Error deleting user by id: ${id}: ${e.message}`,
+                    'SERVER_ERROR',
+                    {}
+                );
+            }
         },
     }
 };
--- src/graphql/typedefs.ts
+++ src/graphql/typedefs.ts
@@ -26,5 +26,34 @@ export const graphqlTypeDefs = gql`
 
     type Mutation {
         createUser(input: UserCreateInput): User
+        updateUser(id: String!, input: UserCreateInput): User
+        deleteUser(id: String!): DeleteResult
+    }
+    
+    type DeleteResult {
+        result: String        
     }
 `;
+
+
+export interface User {
+    id: string,
+    name: string,
+    dob: string | null | undefined,
+    address: string | null | undefined,
+    description: string | null | undefined,
+    imageUrl: string | null | undefined,
+    createdAt: string | null | undefined,
+    updatedAt: string | null | undefined
+}
+
+
+import { ApolloError } from 'apollo-server-errors';
+
+export class AppError extends ApolloError {
+  constructor(message: string, code: string) {
+    super(message, code);
+
+    Object.defineProperty(this, 'name', { value: 'AppError' });
+  }
+}
